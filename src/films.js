efbdd5f046d12c623daad45df9ab7be0d9ed7d70
tonimjdev <tonimdev@gmail.com>
2022-07-29 18:00:12 +0200

    Exercicis 1-2-3

--- src/films.js
+++ src/films.js
@@ -1,46 +1,44 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  // Amb "map" recorrem l'array d'objectes i retornem un nou array amb el director
+  let result = array.map((x) => x.director);
+  console.log('EXERCICE 1 ->', result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  // Amb "filter" retornem un array amb les pelicules del director triat
+  let result = array.filter((x) => x.director === director);
+  console.log('EXERCICE 2 ->', result);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
+// Aprofitem la funció "getMoviesFromDirector()" per fer array amb les pelicules del director triat
 function moviesAverageOfDirector(array, director) {
-  
+  let moviesDirector = getMoviesFromDirector(array, director);
+  // Amb "reduce" obtenim la puntuació mitjana sumant puntuacions. Després dividim per la longitud de l'array
+  let result =
+    moviesDirector.reduce((a, b) => a + b.score, 0) / moviesDirector.length;
+  console.log('EXERCICE 3 ->', result.toFixed(2));
+  return Number(result.toFixed(2));
 }
 
-// Exercise 4:  Alphabetic order by title 
-function orderAlphabetically(array) {
-  
-}
+// Exercise 4:  Alphabetic order by title
+function orderAlphabetically(array) {}
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
-}
+function orderByYear() {}
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-
-}
+function moviesAverageByCategory() {}
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
-
-}
+function hoursToMinutes() {}
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  
-}
-
-
+function bestFilmOfYear() {}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -53,6 +51,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     moviesAverageByCategory,
     hoursToMinutes,
-    bestFilmOfYear,
+    bestFilmOfYear
   };
 }
